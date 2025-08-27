60ebecf0232666fcf1a9da48cf093be1dec9576e
muyajid <kamuyazidkan@gmail.com>
2025-08-27 10:47:17 +0700

    feat: add midleware

--- src/midleware/auth-midleware.js
+++ src/midleware/auth-midleware.js
@@ -1,16 +1,16 @@
 import JWT from "jsonwebtoken";
 import dotenv from "dotenv";
-import logger from "../application/looger-app";
+import logger from "../application/looger-app.js";
 
 dotenv.config();
 
-function jwtAuth(req, res , next) {
+function jwtAuth(req, res, next) {
     const authorization = req.headers.authorization;
 
     if (!authorization) {
         logger.warn(`Auth proces failed: missing authroization headers`);
         return res.status(401).json({
-            message: `Un authorized missing authorization headers`
+            message: `Unauthorized missing authorization headers`
         });
     };
 
--- src/router/private-api-router.js
+++ src/router/private-api-router.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { addProductController } from "../controller/product.controller.js";
+import jwtAuth from "../midleware/auth-midleware.js";
 const privateRouter = express.Router();
 
-privateRouter.post("/api/v1/product/add", express.json(), addProductController);
+privateRouter.post("/api/v1/product/add", express.json(), jwtAuth, addProductController);
 
 export default privateRouter;
